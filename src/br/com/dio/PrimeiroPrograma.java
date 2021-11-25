a2b5b7ff96699c925f89bca37642e38b46828c3d
Dgley Ramos <dgleysilva@gmail.com>
2021-11-25 01:11:55 -0800

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato("roberto", "branco", 5);
+        System.out.println(gato);
+
+    }
+}
